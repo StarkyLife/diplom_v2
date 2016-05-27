48cffea74209e33fb39d1febbc408ebde7e86fcd
StarkyLife <ilshat.ismagilov2014@gmail.com>
2016-05-28 02:49:58 +0300

    GUI - first commit

--- SchemaAdmin.dpr
+++ SchemaAdmin.dpr
@@ -6,18 +6,20 @@ program SchemaAdmin;
 
 uses
   Forms,
-  MainPage in 'GUI\MainPage.pas' {Form7},
+  MainPage in 'GUI\MainPage.pas' {MainForm},
   jwawinldap in 'Modules\jwawinldap.pas',
   LDAPClientUnit in 'Modules\LDAPClientUnit.pas',
   ADSchemaUnit in 'Modules\ADSchemaUnit.pas',
   HelpUnit_LDAPClient in 'Modules\HelpUnit_LDAPClient.pas',
   ADSchemaTypes in 'Modules\ADSchemaTypes.pas',
-  ADSchemaHelpUnit in 'Modules\ADSchemaHelpUnit.pas';
+  ADSchemaHelpUnit in 'Modules\ADSchemaHelpUnit.pas',
+  AddPage in 'GUI\AddPage.pas' {Form1};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm7, Form7);
+  Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
